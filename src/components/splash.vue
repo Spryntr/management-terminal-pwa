7ffe40727dc5550ced841005e9a64744a8e9c629
akohjesse <akohgodwinjesse@gmail.com>
2021-09-29 22:30:36 +0100

    init WIP

---
+++ src/components/splash.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="wrap">
+       <div class="logo image_small">
+          <img src="@/assets/logo.svg" alt="">
+       </div>
+       <div class="splash_graphic">
+           <div class="img">
+               <img :src="splash" alt="">
+           </div>
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+name: "splash",
+props:{
+    name: 'splash',
+    type: String
+}
+}
+</script>
+
+<style lang="scss" scoped>
+ .wrap{
+     width:60%;
+     background:#121417;
+ }
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrap">
+      <Splash/>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Login',
+components:{
+    Splash: ()=> import('../components/splash.vue')
+}
+}
+</script>
+
+<style>
+
+</style>
